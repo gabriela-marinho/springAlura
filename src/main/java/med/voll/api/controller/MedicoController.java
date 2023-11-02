d0f674d0e7ec87e8e6c3b8180782757e42588a7e
gabrielaBarreto <g.barreto.marinho@gmail.com>
2023-11-02 09:52:16 -0300

    inserido primeiro dado no banco atraves do postman

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public class MedicoController {
     //injeção de dependencias
     @Autowired
     private MedicoRepository medicoRepository;
+    @Transactional
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
         //atraves do requestbody consigo pegar o jason q passei no postman colocar na variavel
