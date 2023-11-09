8c9db73898fc81a72d4047bd51192e9016cf22f1
GabrielaPaiva <gabriela@redeis.app.br>
2023-11-09 13:46:01 -0300

    FEAT  paginação e colocação do id no record

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,10 +5,11 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "medicos")
@@ -27,11 +28,11 @@ public class MedicoController {
 @GetMapping
 //   @Transactional nao irá precisar dessa anotation pois nao estou salvando,
 //   atualizando ou exluindo informaçoes, só estou puxando as informaçoes ja existentes
-    public List<DadosListagemMedico> listar(){
+    public Page<DadosListagemMedico> listar(@PageableDefault(size=2, sort={"nome"}) Pageable pageable){
         //no metodo acima eu retorno uma lista dos dados dos medicos pelo record
         //mas o retorno devolve uma list de medico e nao de DadosListagemMedico
     //logo vou converter de medico para DadosListagemMedico com o MAP, atraves do stream.map().tolist()
-        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+        return medicoRepository.findAll(pageable).map(DadosListagemMedico::new);
         //DadosListagemMedico precisa ter dentro dele um construtor que receba o objeto do
     // tipo medico, logo precisa criar esse construtor dentro do record DadosListagemMedico,
     // que recebe como parametro um objeto do tipo medico
